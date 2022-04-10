8dbb399cd608c6629434039e28296215a27aec14
pqx <l.r.slegt@gmail.com>
2022-04-10 13:22:30 +0200

    addition added

--- main.cpp
+++ main.cpp
@@ -1,7 +1,12 @@
 #include <iostream>
 
 int main(){
-    
+    //addition
+    int number1{2};
+    int number2{7};
+
+    int result = number1 + number2;
+    std::cout << "result: " << result << std::endl;
 
     return 0;
 }
